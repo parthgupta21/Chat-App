349cb39a6f22049859448ec967a23c956dabd0d9
Parth <20parthgupta03@gmail.com>
2024-03-27 23:50:58 +0530

    Entire Backend

---
+++ backend/models/conversation.model.js
@@ -0,0 +1,17 @@
+import mongoose from 'monngoose';
+
+const conversationSchema = new mongoose.Schema({
+    participants: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        }
+    ],
+    messages:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Message',
+            default:[],
+        },
+    ],
+}, { timestamps: true });
---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+dotenv.config();
+
+app.use(express.json()); // to parse the incoming requests with json payloads(frome req.body)
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+
+
+// app.get("/", (req, res) => {
+//     // route http://localhost:5000/
+//     res.send("Hello World!!")
+// })
+
+
+
+app.listen(5000, () => {
+    connectToMongoDB();
+    console.log(`Server Running on port ${PORT}`);
+}); 
